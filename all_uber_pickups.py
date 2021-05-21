db0e9e22b151afa041aad1f245a1173ebc315264
oscar <oscarjun.genix@epfedu.fr>
2021-05-21 23:31:00 +0200

    commit

--- all_uber_pickups.py
+++ all_uber_pickups.py
@@ -28,30 +28,30 @@ Data_Url= ('https://s3-us-west-2.amazonaws.com/''streamlit-demo-data/uber-raw-da
 
 @st.cache
 def load_data(nrows):
-    data= pd.read_csv(Data_Url, nrows=nrows)
+    data2= pd.read_csv(Data_Url, nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[Date_Column] = pd.to_datetime(data[Date_Column])
-    return data 
+    data2.rename(lowercase, axis='columns', inplace=True)
+    data2[Date_Column] = pd.to_datetime(data2[Date_Column])
+    return data2
 
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+data2 = load_data(10000)
 data_load_state.text('Done! ')
 
 if st.checkbox('Show raw data'): 
     st.subheader('Raw data')
-    st.write(data)
+    st.write(data2)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[Date_Column].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data2[Date_Column].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[Date_Column].dt.hour == hour_to_filter]
+filtered_data = data2[data2[Date_Column].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at{hour_to_filter}:00')
 st.map(filtered_data)
 
 
 if st.checkbox('Wanna see all the pickups during today? '): 
     st.subheader('Map of all pickups')
-    st.map(data)
+    st.map(data2)
